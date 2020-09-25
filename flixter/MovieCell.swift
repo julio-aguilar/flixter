5cfeb0d4ddbbb8eb60c311bb9b8feb270d02c820
Julio Aguilar <julio.aguilar@upr.edu>
2020-09-24 20:04:04 -0400

    finished main part of project

---
+++ flixter/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flixter
+//
+//  Created by Julio Leonardo Aguilar Colon on 9/24/20.
+//  Copyright © 2020 julio.aguilar@live.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
